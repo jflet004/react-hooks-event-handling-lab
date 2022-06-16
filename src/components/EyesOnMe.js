98e9265f1ab962a3108a17d74ed518e3cd2f95b5
Jose Fletes <joseangelfletes@gmail.com>
2022-06-16 12:33:57 -0700

    commit tree 0d23a9cf6c2ebc9817e166eccab7aed7cec5e533

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,15 @@
 // Code EyesOnMe Component Here
+
+function EyesOnMe() {
+
+  const handleFocus = (e) => console.log("Good!")
+  const handleBlur = (e) => console.log("Hey! Eyes on me!")
+
+  return <button
+    onFocus={handleFocus}
+    onBlur={handleBlur}
+  >Eyes on me</button>
+
+}
+
+export default EyesOnMe
